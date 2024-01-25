701b61262231aae87f01e391bf1199749d66d25b
Marcin27141 <marcin.tkocz@op.pl>
2024-01-25 09:51:00 +0100

    finished renewal repository tests, started renewal controller tests

---
+++ LibraryApp.Tests/ControllersTests/RenewalControllerTest.cs
@@ -0,0 +1,71 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using LibraryApp.Models.Database.Entities;
+using LibraryApp.Models.Database;
+using LibraryApp.Models.Repositories.Renewals.RenewalCreator;
+using LibraryApp.Models.Repositories.Renewals.RenewalValidators;
+using LibraryApp.Models.Repositories.Renewals;
+using LibraryApp.Models.Specifications.RenewalSpecification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryApp.Controllers;
+using LibraryApp.Models.Repositories.Rentals;
+using Microsoft.AspNetCore.Mvc;
+using LibraryApp.Models.ViewModels;
+
+namespace LibraryApp.Tests.ControllersTests
+{
+    public class RenewalControllerTest
+    {
+        private IRenewalRepository _renewalRepository;
+        private IRentalRepository _rentalRepository;
+        public RenewalControllerTest()
+        {
+            _renewalRepository = A.Fake<IRenewalRepository>();
+            _rentalRepository = A.Fake<IRentalRepository>();
+        }
+
+        private RenewalController GetRenewalController() =>
+            new RenewalController(_renewalRepository, _rentalRepository);
+
+        [Fact]
+        public void RenewalController_Index_NotFoundOnNullRental()
+        {
+            //Arrange
+            int rentalId = -1;
+            A.CallTo(() => _rentalRepository.GetRentalById(rentalId)).Returns(null);
+            var controller = GetRenewalController();
+
+            //Act
+            var actionResult = controller.Index(rentalId);
+
+            //Assert
+            Assert.Equal(typeof(NotFoundResult), actionResult.GetType());
+        }
+
+        [Fact]
+        public void RenewalController_Index_CorrectViewWithPassedRental()
+        {
+            //Arrange
+            var rental = new Rental() { RentalId = -1, ReaderId = Guid.NewGuid().ToString(), BookCopyId = -1 };
+            A.CallTo(() => _rentalRepository.GetRentalById(rental.RentalId)).Returns(rental);
+            var controller = GetRenewalController();
+
+            //Act
+            var actionResult = controller.Index(rental.RentalId);
+
+            //Assert
+            Assert.Equal(typeof(ViewResult), actionResult.GetType());
+            if (actionResult is ViewResult)
+            {
+                var viewResult = actionResult as ViewResult;
+                var model = (ConfirmRenewalViewModel)viewResult?.ViewData?.Model;
+                Assert.Equal(model?.Rental, rental);
+            }
+                
+        }
+    }
+}
--- LibraryApp.Tests/RepositoriesTests/RenewalRepositoryTests.cs
+++ LibraryApp.Tests/RepositoriesTests/RenewalRepositoryTests.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using LibraryApp.Models.Database.Entities;
 using FluentAssertions;
+using LibraryApp.Models.ViewModels;
 
 namespace LibraryApp.Tests.RepositoriesTests
 {
@@ -20,12 +21,31 @@ namespace LibraryApp.Tests.RepositoriesTests
         private IRenewalCreator _renewalCreator;
         private IRenewalSpecification _renewalSpecification;
         private IEnumerable<IRenewalValidator> _renewalValidators;
+        private const int TEST_RENTAL_ID = 100;
         public RenewalRepositoryTests()
         {
             _context = FakeDbContext.FakeDbContext.GetFakeDbContext();
             _renewalCreator = A.Fake<IRenewalCreator>();
             _renewalSpecification = A.Fake<IRenewalSpecification>();
             _renewalValidators = Enumerable.Empty<IRenewalValidator>();
+
+            if (_context.Rentals.Find(TEST_RENTAL_ID) == null)
+                AddTestRental();
+        }
+
+        private void AddTestRental()
+        {
+            var today = DateOnly.FromDateTime(DateTime.Today);
+            _context.Rentals.Add(new Rental()
+            {
+                RentalId = TEST_RENTAL_ID,
+                RentalDate = today,
+                OriginalReturnDeadline = today.AddDays(30),
+                CurrentDeadline = today.AddDays(30),
+                ReaderId = Guid.NewGuid().ToString(),
+                BookCopyId = TEST_RENTAL_ID
+            });
+            _context.SaveChanges();
         }
 
         private RenewalRepository GetRenewalRepository() =>
@@ -34,8 +54,8 @@ namespace LibraryApp.Tests.RepositoriesTests
         [Theory]
         [InlineData(0)]
         [InlineData(1)]
-        [InlineData(2)]
-        public async Task RenewalRepository_GetRemainingRenewals_AlwaysInRange(int maxRenewals)
+        [InlineData(8)]
+        public void RenewalRepository_GetRemainingRenewals_AlwaysInRange(int maxRenewals)
         {
             //Arrange
             A.CallTo(() => _renewalSpecification.MaxRenewalsPerRental).Returns(maxRenewals);
@@ -52,6 +72,62 @@ namespace LibraryApp.Tests.RepositoriesTests
 
             //Assert
             remainingList.Should().AllSatisfy(x => Assert.InRange(x, 0, maxRenewals));
+        }
+
+        [Theory]
+        [InlineData(0, 2, 2)]
+        [InlineData(1, 4, 3)]
+        [InlineData(20, 41, 21)]
+        public void RenewalRepository_GetRemainingRenewals_CorrectValue(int currentRenewals, int maxRenewals, int expected)
+        {
+            //Arrange
+            A.CallTo(() => _renewalSpecification.MaxRenewalsPerRental).Returns(maxRenewals);
+            var repo = GetRenewalRepository();
+            var rental = new Rental() { Renewals = Enumerable.Repeat(new Renewal(), currentRenewals).ToList() };
+
+            //Act
+            int remaining = repo.GetRemainingRenewals(rental);
+
+            //Assert
+            remaining.Should().Be(expected);
+        }
+
+        [Fact]
+        public void RenewalRepository_IsValidForRenewal_TrueWhenNoValidators()
+        {
+            //Arrange
+            var repo = GetRenewalRepository();
+            var rental = new Rental();
+
+            //Act
+            var validityCheck = repo.IsValidForRenewal(rental);
+
+            //Assert
+            validityCheck.IsValidForRenewal.Should().Be(true);
+        }
+
+        [Theory]
+        [InlineData(30)]
+        [InlineData(10)]
+        [InlineData(1)]
+        [InlineData(100)]
+        public void RenewalRepository_RenewRental_CorrectlyRenewed(int renewalSpan)
+        {
+            //Arrange
+            A.CallTo(() => _renewalSpecification.RenewalSpanInDays).Returns(renewalSpan);
+            var repo = GetRenewalRepository();
+            var rental = _context.Rentals.Find(TEST_RENTAL_ID);
+            var previousDeadline = rental?.CurrentDeadline;
+            A.CallTo(() => _renewalCreator.CreateRenewal(rental)).Returns(
+                new Renewal() { NewReturnDeadline = previousDeadline.Value.AddDays(renewalSpan)}
+                );
+
+            //Act
+            var validityCheck = repo.RenewRental(TEST_RENTAL_ID);
+
+            //Assert
+            var newDeadline = _context.Rentals.Find(TEST_RENTAL_ID)?.CurrentDeadline;
+            newDeadline.Should().Be(previousDeadline.Value.AddDays(renewalSpan));
         }
     }
 }
